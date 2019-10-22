97495845f7faea73a427e52b948d225abfed02b8
fx19870419 <fx19870419@163.com>
2019-10-22 20:46:18 +0800

    Update 0.1.py

--- 0.1.py
+++ 0.1.py
@@ -1,8 +1,29 @@
-import PyPDF2
-pdfFileobj = open('guoyan.pdf','rb')
-reader = PyPDF2.PdfFileReader(pdfFileobj)
-pages = []
-for i in range(0,reader.numPages):
-    page = reader.getPage(i)
-    pages.append(page.extractText())
-print(pages)
+from wand.image import Image
+
+'''
+pdf = Image(filename='guoyan.pdf',resolution =300)
+jpeg = pdf.convert('jpg')
+
+
+images=[]
+for i in jpeg.sequence:
+    page = Image(image=i)
+    images.append(page.make_blob('jpg'))
+
+
+
+i = 0
+for img in images:
+    ff = open(str(i)+'.jpg','wb')
+    ff.write(img)
+    ff.close()
+    i += 1
+'''
+filename="guoyan.pdf"
+with(Image(filename=filename, resolution=120)) as source: 
+    images = source.sequence
+    pages = len(images)
+    for i in range(pages):
+        n = i + 1
+        newfilename = filename[:-4] + str(n) + '.jpeg'
+        Image(images[i]).save(filename=newfilename)
